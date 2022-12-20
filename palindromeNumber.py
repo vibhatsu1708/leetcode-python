55efd750cec915b99a599aa2360ebe1ee8a1db1f
vibhatsu08 <mistryvedant17@gmail.com>
2022-12-20 23:52:43 +0530

    'palindromeNumber' question on Leetcode.

---
+++ palindromeNumber.py
@@ -0,0 +1,6 @@
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+def palindromeNumber (x) :
+    temp = str(x)
+    return (temp == temp[::-1])
+
+print(palindromeNumber(-121))
