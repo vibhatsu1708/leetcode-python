8d946e29795939861333090db9484d35268733cc
vibhatsu08 <mistryvedant17@gmail.com>
2023-01-23 23:44:21 +0530

    'Search Insert Position' problem on Leetcode.

--- insertPosition.py
+++ insertPosition.py
@@ -3,10 +3,14 @@
 # You must write an algorithm with O(log n) runtime complexity.
 
 def searchInsert (nums, target) :
-    for i in range (len(nums)) :
-        if nums[i] == target :
-            return i
-        if nums[i] > target :
-            return i
-    return len(nums)
-print(searchInsert(nums = [1,3,5,6], target = 7))
+    start, end = 0, len(nums)-1
+    while (start <= end) :
+        mid = (start + end)//2
+        if nums[mid] == target :
+            return mid
+        elif nums[mid] > target :
+            end = mid - 1
+        elif nums[mid] < target :
+            start = mid + 1
+    return end + 1
+print(searchInsert(nums = [1,3], target = 2))
