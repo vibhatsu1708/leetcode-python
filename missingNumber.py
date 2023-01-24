47af6ec1f9a49c06571dc977f4b9583df58c9c0a
vibhatsu08 <mistryvedant17@gmail.com>
2023-01-25 02:09:03 +0530

    'Missing Number' problem on Leetcode.

--- missingNumber.py
+++ missingNumber.py
@@ -2,14 +2,11 @@
 # Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
 def missingNumber (nums) :
-    # nums.append(-1)
-    numsNew = []
-    length = len(nums)
-    for i in range (0, length+1) :
-        numsNew.append(i)
-    nums.append("")
-    for i in range (len(numsNew)) :
-        if numsNew[i] not in nums :
-            return numsNew[i]
+    sum = 0
+    for i in range (0, len(nums)+1) :
+        sum += i
+    for i in range (len(nums)) :
+        sum -= nums[i]
+    return sum
         
 print(missingNumber(nums = [9,6,4,2,3,5,7,0,1]))
