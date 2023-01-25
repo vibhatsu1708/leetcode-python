e17b769ff1dc1877898a7f8d06fccf3d5ab73786
vibhatsu08 <mistryvedant17@gmail.com>
2023-01-25 05:41:13 +0530

    'A Number After a Double Reversal' problem on Leetcode.

--- aNumberAfterDoubleReversal.py
+++ aNumberAfterDoubleReversal.py
@@ -4,19 +4,7 @@
 # Given an integer num, reverse num to get reversed1, then reverse reversed1 to get reversed2. Return true if reversed2 equals num. Otherwise return false.
 
 def isSameAfterReversals (num) :
-    temp = num
-    reverse, doubleReverse = 0, 0
-    while (temp != 0) :
-        remainder = temp % 10
-        reverse = reverse*10 + remainder
-        temp //= 10
-    temp = reverse
-    while (temp != 0) :
-        remainder = temp % 10
-        doubleReverse = doubleReverse*10 + remainder
-        temp //= 10
-    if doubleReverse == num :
-        return True
-    else :
-        return False
+    reverse = str(num)[::-1]
+    doubleReverse = str(int(reverse))[::-1]
+    return (int(doubleReverse) == num)
 print(isSameAfterReversals(num = 0))
