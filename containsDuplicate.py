387966667764699687a1cc5a26f70f4170a0a6f3
vibhatsu08 <mistryvedant17@gmail.com>
2023-01-25 23:26:53 +0530

    'Contains Duplicate' problem on Leetcode.

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,17 +1,26 @@
 # Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 def check (nums) :
-    counts = {}
+    # counts = {}
     
-    # count the num of occurences of each element.
-    for num in nums :
-        if num not in counts :
-            counts[num] = 1
-        else :
-            counts[num] += 1
+    # # count the num of occurences of each element.
+    # for num in nums :
+    #     if num not in counts :
+    #         counts[num] = 1
+    #     else :
+    #         counts[num] += 1
             
-    # return true if an element appears more than once.
-    for count in counts.values() :
-        if count > 1 :
+    # # return true if an element appears more than once.
+    # for count in counts.values() :
+    #     if count > 1 :
+    #         return True
+    # return False
+
+    duplicates = {}
+    for num in nums :
+        if num not in duplicates :
+            duplicates[num] = 1
+            continue
+        if num in duplicates :
             return True
     return False
 
