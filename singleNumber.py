540dc27586d466d34471a2bd8ac1487af97d437c
vibhatsu08 <mistryvedant17@gmail.com>
2023-01-29 19:08:47 +0530

    'Single Number' problem on Leetcode.

--- singleNumber.py
+++ singleNumber.py
@@ -2,14 +2,10 @@
 # Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 # You must implement a solution with a linear runtime complexity and use only constant extra space.
 
+# This solution uses constant extra space. 
 def singleNumber (nums) :
-    count = {}
+    result = 0
     for num in nums :
-        if num in count :
-            count[num] += 1
-        else :
-            count[num] = 1
-    for i, key in enumerate(count) :
-        if count[key] == 1 :
-            return key
-print(singleNumber(nums=[1]))
+        result ^= num
+    return result
+print(singleNumber(nums=[-1,-1,-2]))
