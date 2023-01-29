bf35d66356be1bcca860261d756becd6734f4d81
vibhatsu08 <mistryvedant17@gmail.com>
2023-01-29 19:00:39 +0530

    'Single Number II 'problem on Leetcode.

--- singleNumber2.py
+++ singleNumber2.py
@@ -3,13 +3,19 @@
 # You must implement a solution with a linear runtime complexity and use only constant extra space.
 
 def singleNumber (nums) :
-    count = {}
-    for i in range (len(nums)) :
-        if nums[i] not in count :
-            count[nums[i]] = 1
-        else :
-            count[nums[i]] += 1
-    for i, value in enumerate (count) :
-        if count[value] == 1 :
-            return value
+    # count = {}
+    # for i in range (len(nums)) :
+    #     if nums[i] not in count :
+    #         count[nums[i]] = 1
+    #     else :
+    #         count[nums[i]] += 1
+    # for i, value in enumerate (count) :
+    #     if count[value] == 1 :
+    #         return value
+    
+    once, twice = 0, 0
+    for num in nums :
+        once = (once ^ num) & (~twice)
+        twice = (twice ^ num) & (~once)
+    return once
 print(singleNumber(nums=[0,1,0,1,0,1,99]))
