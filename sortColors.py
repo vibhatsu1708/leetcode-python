4d29c2913aa4efc710a543b0d8072c910f6d421f
vibhatsu08 <mistryvedant17@gmail.com>
2023-02-02 04:25:14 +0530

    'Sort Colors' problem on Leetcode.

--- sortColors.py
+++ sortColors.py
@@ -4,12 +4,31 @@
 # You must solve this problem without using the library's sort function.
 
 def sortColors (nums) :
-    # Used the selection sort algorithm to achieve the result.
+    # best is Selection Sort, runtime wise
+    # for i in range (len(nums)) :
+    #     minIndex = i
+    #     for j in range (i+1, len(nums)) :
+    #         if nums[j] < nums[minIndex] :
+    #             minIndex = j
+    #     nums[i], nums[minIndex] = nums[minIndex], nums[i]
+    # return nums
+    
+    # Bubble Sort
+    # swapped = True
+    # while (swapped) :
+    #     swapped = False
+    #     for i in range (len(nums)-1) :
+    #         if nums[i] > nums[i+1] :
+    #             nums[i], nums[i+1] = nums[i+1], nums[i]
+    #             swapped = True
+    # return nums
+            
+    # Insertion Sort
     for i in range (len(nums)) :
-        minIndex = i
-        for j in range (i+1, len(nums)) :
-            if nums[j] < nums[minIndex] :
-                minIndex = j
-        nums[i], nums[minIndex] = nums[minIndex], nums[i]
+        currentIndex = i
+        while (currentIndex > 0 ) and (nums[currentIndex-1] > nums[currentIndex]) :
+            nums[currentIndex], nums[currentIndex-1] = nums[currentIndex-1], nums[currentIndex]
+            currentIndex -= 1
     return nums
+     
 print(sortColors(nums = [2,0,2,1,1,0]))
