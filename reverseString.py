8355b4e8ffccfb5c4c4123f60f93024ed95686d8
vibhatsu08 <mistryvedant17@gmail.com>
2023-02-02 16:13:46 +0530

    'Reverse String' problem on Leetcode.

--- reverseString.py
+++ reverseString.py
@@ -3,25 +3,10 @@
 # You must do this by modifying the input array in-place with O(1) extra memory.
 
 def reverseString (s) :
-    for i in range (len(s)) :
-        ch = s.pop()
-        s.insert(i, ch)
+    left, right = 0, len(s)-1
+    while (left < right) :
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
     return s
-
-# functioning of the program, 
-# loops through the array, and individually pops each and every element, and stores that element in a variable.
-# once stored, that variable's value is stored at the ith position of the same array from the beginning.
-    
 print(reverseString(s=["h","e","l","l","o"]))
-
-"""
-Example 1:
-Input: s = ["h","e","l","l","o"]
-Output: ["o","l","l","e","h"]
-"""
-
-"""
-Example 2:
-Input: s = ["H","a","n","n","a","h"]
-Output: ["h","a","n","n","a","H"]
-"""
