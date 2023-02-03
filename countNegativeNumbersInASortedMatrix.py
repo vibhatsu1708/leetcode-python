c508d874e88997e8da359bea4fa95d9c4cc0be96
vibhatsu08 <mistryvedant17@gmail.com>
2023-02-03 12:21:30 +0530

    'Count Negative Numbers in a Sorted Matrix' problem on Leetcode.

--- countNegativeNumbersInASortedMatrix.py
+++ countNegativeNumbersInASortedMatrix.py
@@ -1,11 +1,19 @@
 # Count Negative Numbers in a Sorted Matrix
 # Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
-
-def countNegatives (grid) :
+def countNegatives(grid):
+    rows = len(grid)
+    columns = len(grid[0])
+    i = rows - 1
+    j = 0
     count = 0
-    for i in range (len(grid)) :
-        for j in range (len(grid[i])) :
-            if grid[i][j] < 0 :
-                count += 1
+    while (i >= 0 and j < columns) :
+        if grid[i][j] < 0 :
+            count += (columns - j)
+            i -= 1
+        else :
+            j += 1
     return count
-print(countNegatives(grid = [[3,2],[1,0]]))
+print(countNegatives(grid = [[4,3,2,-1],
+                             [3,2,1,-1],
+                             [1,1,-1,-2],
+                             [-1,-1,-2,-3]]))
