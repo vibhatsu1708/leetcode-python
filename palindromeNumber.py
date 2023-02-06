9607ed9adc596f1e58637c3dfec75747d64dc34f
vibhatsu08 <mistryvedant17@gmail.com>
2023-02-06 09:28:56 +0530

    'Palindrome Number' program on Leetcode.

--- palindromeNumber.py
+++ palindromeNumber.py
@@ -1,6 +1,15 @@
+# Palindrome Number
 # Given an integer x, return true if x is a palindrome, and false otherwise.
-def palindromeNumber (x) :
-    temp = str(x)
-    return (temp == temp[::-1])
 
-print(palindromeNumber(-121))x
+def palindromeNumber (x) :
+    temp = abs(x)
+    palindromeNum = 0
+    while (temp > 0) :
+        remainder = temp%10
+        palindromeNum = palindromeNum*10 + remainder
+        temp //= 10
+    if palindromeNum == x :
+        return True
+    else :
+        return False
+print(palindromeNumber(10))
