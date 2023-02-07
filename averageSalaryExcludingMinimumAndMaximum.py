7dae2845be9901f586a48386dc4f403599dc7b5b
vibhatsu08 <mistryvedant17@gmail.com>
2023-02-07 15:10:32 +0530

    'Average Salary Excluding the Minimum and Maximum Salary' problem on Leetcode.

--- averageSalaryExcludingMinimumAndMaximum.py
+++ averageSalaryExcludingMinimumAndMaximum.py
@@ -3,10 +3,20 @@
 # Return the average salary of employees excluding the minimum and maximum salary. Answers within 10-5 of the actual answer will be accepted.
 
 def average (salary) :
-    minSalary = min(salary)
-    maxSalary = max(salary)
-    sum = 0
+    # minSalary = min(salary)
+    # maxSalary = max(salary)
+    # sum = 0
+    # for num in salary :
+    #     sum += num
+    # return (sum - minSalary - maxSalary)/(len(salary)-2)
+    
+    minSalary, maxSalary = salary[0], salary[0]
+    sumSalary = 0
     for num in salary :
-        sum += num
-    return (sum - minSalary - maxSalary)/(len(salary)-2)
+        if num < minSalary :
+            minSalary = num
+        if num > maxSalary :
+            maxSalary = num
+        sumSalary += num
+    return (sumSalary - minSalary - maxSalary) / (len(salary) - 2)
 print(average(salary=[4000,3000,1000,2000]))
