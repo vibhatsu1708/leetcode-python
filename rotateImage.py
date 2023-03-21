1fe94d11df9caf7aa088acef3b7d922e169d7a0e
vibhatsu08 <mistryvedant17@gmail.com>
2023-03-21 23:57:37 +0530

    'Rotate Image' problem on Leetcode.

--- rotateImage.py
+++ rotateImage.py
@@ -1,6 +1,6 @@
 # Rotate Image
 # You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
-# You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+# You have to rotate the image in-place,which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
 
 def rotate (matrix) :
     # find transpose of a matrix.
@@ -10,7 +10,11 @@ def rotate (matrix) :
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
     # swap the elements from 0 till halfway, and from halfway till the end. 
     for i in range (len(matrix)) :
-        for j in range (len(matrix)//2) :
-            matrix[i][j], matrix[i][len(matrix)-j-1] = matrix[i][len(matrix)-j-1], matrix[i][j]
+        start = 0
+        end = len(matrix[0])-1
+        while (start < end) :
+            matrix[start], matrix[end] = matrix[end], matrix[start]
+            start += 1
+            end -= 1
     return matrix
 print(rotate(matrix = [[1,2,3],[4,5,6],[7,8,9]]))
