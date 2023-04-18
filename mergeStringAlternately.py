23caf6335f86841269dac1f4fe51d686c73e5b0c
vibhatsu08 <mistryvedant17@gmail.com>
2023-04-18 20:10:11 +0530

    'Merge Strings Alternately' problem on Leetcode.

--- mergeStringAlternately.py
+++ mergeStringAlternately.py
@@ -2,15 +2,18 @@
 # You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
 # Return the merged string.
 
-def mergeAlternately (word1, word2) :
-    resultStr = ""
-    i = 0
-    while (i<len(word1) or i<len(word2)) :
-        if i < len(word1) :
-            resultStr += word1[i]
-        if i < len(word2) :
-            resultStr += word2[i]
+def mergeAlternately(word1, word2):
+    newstr = ""
+    i, j = 0, 0
+    while i < len(word1) and j < len(word2):
+        newstr += word1[i]
+        newstr += word2[j]
         i += 1
-    return resultStr
-        
-print(mergeAlternately(word1="abc", word2="pqr"))
+        j += 1
+    if i != len(word1):
+        return newstr + word1[i:]
+    elif j != len(word2):
+        return newstr + word2[j:]
+    return newstr
+
+print(mergeAlternately(word1="abcd", word2="pq"))
