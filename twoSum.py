507b816a3a6c222d00063c025d067062eba6a291
vibhatsu08 <mistryvedant17@gmail.com>
2023-05-10 06:39:45 +0530

    'Two Sum' problem on Leetcode.

--- twoSum.py
+++ twoSum.py
@@ -1,11 +1,12 @@
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 # You can return the answer in any order.
-def twoSum (nums, target) :
-    for i in range (0, len(nums)) :
-        for j in range (i+1, len(nums)) :
-            if nums[i]+nums[j] == target :
-                return [i, j]
-    return []
-            
-print(twoSum(nums=[3,2,4], target=6))
+
+def twoSum(nums, target):
+    dictMap = {}
+    for index, num in enumerate(nums):
+        diff = target - num
+        if diff in dictMap:
+            return [dictMap[diff], index]
+        dictMap[num] = index
+print(twoSum(nums = [3,2,4], target = 6))
