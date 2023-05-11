c1f8928461a0e40e21176b62169a351daf521a8d
vibhatsu08 <mistryvedant17@gmail.com>
2023-05-11 21:15:52 +0530

    'Contains Duplicate II' problem on Leetcode.

--- containsDuplicate2.py
+++ containsDuplicate2.py
@@ -2,12 +2,10 @@
 # Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 
 def containsNearbyDuplicate (nums, k) :
-    seen = {}
-    for i in range (len(nums)) :
-        if nums[i] not in seen :
-            seen[nums[i]] = i
-        else :
-            if i - seen[nums[i]] <= k :
-                return True
+    count = {}
+    for index, num in enumerate(nums):
+        if num in count and abs(index - count[num]) <= k:
+            return True
+        count[num] = index
     return False
 print(containsNearbyDuplicate(nums = [1,2,3,1,2,3], k = 2))
