0345340b56bbd922466a1a8aa8c57e5d5ffa394d
vibhatsu08 <mistryvedant17@gmail.com>
2023-05-11 07:55:13 +0530

    'Valid Palindrome' problem on Leetcode.

--- validPalindrome.py
+++ validPalindrome.py
@@ -2,16 +2,13 @@
 # A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 # Given a string s, return true if it is a palindrome, or false otherwise.
 
-def isPalindrome (s) :
+def isPalindrome(s):
     s = s.lower()
-    str, revstr = "", ""
-    for i in range (len(s)) :
-        if s[i] >= 'a' and s[i] <= 'z' or s[i] >= '0' and s[i] <= '9' :
-            str += s[i]
-        revstr = str[::-1]
-    if revstr == str :
-        return True
-    else :
-        return False
-print(isPalindrome(s="0P"))
+    newS = ""
+    for i in range(len(s)):
+        if ord(s[i]) >= 97 and ord(s[i]) <= 122 or ord(s[i]) >= 48 and ord(s[i]) <= 57:
+            newS += s[i]
+    revStr = newS[::-1]
+    return newS == revStr
+print(isPalindrome(s = " 0P"))
     
