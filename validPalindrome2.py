86763ee65c898a59fd172be4a2170d04a3a2fd91
vibhatsu08 <mistryvedant17@gmail.com>
2023-05-11 10:50:18 +0530

    'Valid Palindrome II' problem on Leetcode.

--- validPalindrome2.py
+++ validPalindrome2.py
@@ -2,22 +2,12 @@
 # Given a string s, return true if the s can be palindrome after deleting at most one character from it.
 
 def validPalindrome (s) :
-    # Time limit exceeded
-    # prefix, suffix = "", ""
-    # for i in range (len(s)) :
-    #     prefix = s[0:i]
-    #     suffix = s[i+1:]
-    #     if (prefix+suffix) == (prefix+suffix)[::-1] :
-    #         return True
-    # return False
-    
-    # working solution
     left, right = 0, len(s)-1
-    while (left < right) :
-        if s[left] != s[right] :
-            leftSkip, rightSkip = s[left+1 : right+1], s[left:right]
-            return (leftSkip == leftSkip[::-1] or rightSkip == rightSkip[::-1])
+    while (left < right):
+        if s[left] != s[right]:
+            skipLeft, skipRight = s[left+1:right+1], s[left, right]
+            return (skipLeft == skipLeft[::-1]) or (skipRight == skipRight[::-1])
         left += 1
         right -= 1
     return True
-print(validPalindrome(s = "abca"))
+print(validPalindrome(s = "aba"))
