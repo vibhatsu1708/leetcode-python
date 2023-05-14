238eeb9ed166ad87563e2ce151a65cf6085576dc
vibhatsu08 <mistryvedant17@gmail.com>
2023-05-14 10:50:57 +0530

    'Binary Search' problem on Leetcode.

--- binarySearch.py
+++ binarySearch.py
@@ -3,16 +3,14 @@
 # You must write an algorithm with O(log n) runtime complexity.
 
 def search (nums, target) :
-    if len(nums) == 0 : 
-        return -1
-    start, end = 0, len(nums)-1
-    while (start <= end) :
-        mid = (start + end)//2
-        if nums[mid] == target :
-            return mid
-        elif nums[mid] > target :
-            end = mid - 1
-        elif nums[mid] < target :
-            start = mid + 1
+    left, right = 0, len(nums)-1
+    while (left <= right):
+        middle = (left + right) // 2
+        if nums[middle] == target:
+            return middle
+        elif nums[middle] < target:
+            left = middle + 1
+        else:
+            right = middle - 1
     return -1
 print(search(nums = [2,5], target = 5))
