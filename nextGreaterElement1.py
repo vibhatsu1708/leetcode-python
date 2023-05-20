ffbfa8197403d7f0d09119ace1343ec877bee3af
vibhatsu08 <mistryvedant17@gmail.com>
2023-05-20 17:05:40 +0530

    'Next Greater Element I' problem on Leetcode.

--- nextGreaterElement1.py
+++ nextGreaterElement1.py
@@ -5,18 +5,14 @@
 # Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
 
 def nextGreaterElement(nums1, nums2):
-    result = []
-    for i in range (len(nums1)) :
-        elementIndex = 0
-        for j in range (len(nums2)) :
-            if nums1[i] == nums2[j] :
-                elementIndex = j
+    result = [-1] * len(nums1)
+    for i in range(len(nums1)):
+        index = nums2.index(nums1[i])
+        for j in range(index+1, len(nums2)):
+            if nums2[j] > nums1[i]:
+                result[i] = nums2[j]
                 break
-        for k in range (elementIndex+1,len(nums2)) :
-            if nums2[k] > nums1[i] :
-                result.append(nums2[k])
-                break
-        else :
-            result.append(-1)
+            else:
+                continue
     return result
-print(nextGreaterElement(nums1 = [4,1,2], nums2 = [1,3,4,2]))
+print(nextGreaterElement(nums1 = [1,3,5,2,4], nums2 = [6,5,4,3,2,1,7]))
